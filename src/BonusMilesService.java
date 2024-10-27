bac942d3a980bae7319e916823522ebbbd89e9fa
Arina Udod <audod6150@gmail.com>
2024-10-27 23:52:21 +0300

    Second commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
     public int calculate(int bonus) {
         if (bonus > 0) {
-            bonus = bonus / 20 / 100;
+            bonus = bonus / 20;
         } else {
             bonus = 0;
         }
